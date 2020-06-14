0d8dcc4c50d6606caa49ed75f3664e09c163848c
Austin Noronha <austin_noronha@hotmail.com>
2020-06-14 22:26:42 +0530

    New API for Showing the form and saving the POST data to Mongo with Course Model

--- controllers/course.js
+++ controllers/course.js
@@ -4,6 +4,7 @@
  */
 const express = require("express");
 const mongoose = require("mongoose");
+const uuid = require("uuid");
 
 const CourseModel = mongoose.model("Course");
 const router = express.Router();
@@ -11,13 +12,6 @@ const router = express.Router();
 //App dynamic APIs
 
 router.get("/", (req, res) => {
-  //   var newCourse = new CourseModel();
-  //   newCourse.courseName = "Nodejs 101";
-  //   newCourse.courseId = "2380239";
-  //   newCourse.courseDuration = "20hrs";
-  //   newCourse.courseFee = "1000rs";
-  //   newCourse.save();
-
   CourseModel.find((err, docs) => {
     if (err) {
       return res.status(400).send("MongoDB List Error");
@@ -25,6 +19,36 @@ router.get("/", (req, res) => {
       return res.status(200).render("list", { data: docs });
     }
   }).lean() /*Due to some dev dependencies in express handlebars, we need to use lean() to get JSON object*/;
+});
+
+router.get("/add", (req, res) => {
+  return res.status(200).render("add-course");
+});
+
+router.post("/add", (req, res) => {
+  var courseBody = req.body;
+
+  if (
+    !courseBody.courseName ||
+    !courseBody.courseDuration ||
+    !courseBody.courseFee
+  ) {
+    return res.status(400).send("Please enter course name, suration and fees!");
+  }
+
+  var newCourse = new CourseModel();
+  newCourse.courseName = courseBody.courseName;
+  newCourse.courseId = uuid.v1();
+  newCourse.courseDuration = courseBody.courseDuration;
+  newCourse.courseFee = courseBody.courseFee;
+
+  newCourse.save((err, doc) => {
+    if (err) {
+      return res.status(400).send("Error in adding data, please try again.");
+    } else {
+      return res.redirect("/course/");
+    }
+  });
 });
 
 module.exports = router;
